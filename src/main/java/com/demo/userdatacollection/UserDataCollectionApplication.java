8d9e8399d134456a9967ecd22bc4e31ca39cb805
shrinivasiitg <shrinivas.iitg@gmail.com>
2022-11-23 19:52:16 -0500

    First commit (inspired from https://spring.io/guides/gs/rest-service/#scratch and https://start.spring.io/)

---
+++ src/main/java/com/demo/userdatacollection/UserDataCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.demo.userdatacollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDataCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/userdatacollection/UserDataCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.userdatacollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
